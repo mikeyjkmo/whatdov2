ac73242a58e211d32d04016a99e28f5e4d379332
Mikey Mo <mikeyjkmo@gmail.com>
2022-05-02 15:33:18 +0100

    Use special types in FastAPI entrypoint

--- backend/whatdo2/entrypoints/fast_api.py
+++ backend/whatdo2/entrypoints/fast_api.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pydantic.main import BaseModel
 import dataclasses as dc
 from fastapi import FastAPI
-from typing import Dict, Any
+from typing import Dict, Any, List
 from motor.motor_asyncio import AsyncIOMotorClient
 from whatdo2.config import MONGO_CONNECTION_STR, MONGO_DB_NAME
 
@@ -26,25 +26,50 @@ class DependentTaskPayload(BaseModel):
     id: UUID
 
 
+class DependentTask(BaseModel):
+    id: UUID
+
+
+class Task(BaseModel):
+    id: UUID
+    name: str
+    importance: int
+    task_type: TaskType
+    time: int
+    activation_time: datetime
+    is_active: bool
+    density: float
+    effective_density: float
+    is_prerequisite_for: List[DependentTask]
+
+
+class TaskListReponse(BaseModel):
+    tasks: List[Task]
+
+
+class TaskResponse(BaseModel):
+    task: Task
+
+
 @app.get("/tasks")
-async def task_list() -> Dict[Any, Any]:
+async def task_list() -> TaskListReponse:
     def _clean(t: Dict[Any, Any]) -> Dict[Any, Any]:
         result = t.copy()
         del result["_id"]
         return result
 
-    return {
+    return TaskListReponse.parse_obj({
         "tasks": [
             _clean(t)
             for t in await db.tasks.find()
             .sort("effective_density", -1)
             .to_list(length=None)
         ]
-    }
+    })
 
 
 @app.post("/tasks")
-async def create_task(task: TaskCreationPayload) -> Dict[Any, Any]:
+async def create_task(task: TaskCreationPayload) -> TaskResponse:
     result = await service.create_task(
         name=task.name,
         importance=task.importance,
@@ -52,20 +77,16 @@ async def create_task(task: TaskCreationPayload) -> Dict[Any, Any]:
         task_type=task.task_type,
         activation_time=task.activation_time,
     )
-    return {
-        "task": dc.asdict(result),
-    }
+    return TaskResponse(task=Task.parse_obj(dc.asdict(result)))
 
 
 @app.post("/task/{task_id}/dependent_tasks")
 async def add_dependent_task(
     task_id: UUID,
     dependent_task: DependentTaskPayload,
-) -> Dict[Any, Any]:
+) -> TaskResponse:
     result = await service.add_dependent_task(
         task_id=task_id,
         dependent_task_id=dependent_task.id,
     )
-    return {
-        "task": dc.asdict(result),
-    }
+    return TaskResponse(task=Task.parse_obj(dc.asdict(result)))
