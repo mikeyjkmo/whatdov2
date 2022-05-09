43382e40dabdbcdf1d88fde4fb0db6e191a4f420
Mikey Mo <mikeyjkmo@gmail.com>
2022-05-09 09:26:18 +0100

    Add missing UoW module

---
+++ backend/whatdo2/service_layer/unit_of_work.py
@@ -0,0 +1,21 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio.engine import create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from whatdo2.adapters.sql_task_repository import SQLTaskRepository
+from whatdo2.config import POSTGRES_URI
+
+
+class UnitOfWork:
+    def __init__(self, session: AsyncSession):
+        self.task_repository = SQLTaskRepository(session)
+
+
+@asynccontextmanager
+async def new_uow() -> AsyncGenerator[UnitOfWork, None]:
+    engine = create_async_engine(POSTGRES_URI, echo=True)
+    async with AsyncSession(engine, expire_on_commit=False) as session:
+        yield UnitOfWork(session)
+        await session.commit()
